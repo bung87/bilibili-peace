5e3b8c185e039e9813195b38f695cc3ec838b0df
zhoupeng <zhoupeng@xjgreat.com>
2018-03-28 18:55:25 +0800

    hashchange has beend changed to pushState or replaceState since cannot detect this action just listen click event

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,6 +2,7 @@
 
 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
 var port = chrome.extension.connect();
+
 // 选择目标节点
 var target = document.querySelector('#bofqi');
 var danmakuBtn = ".bilibili-player-video-btn-danmaku";
@@ -9,66 +10,53 @@ var con2 = false;
 var observer;
 
 function closeDanmaku() {
-
   var par = target.firstChild.tagName == "IFRAME" ? document.querySelector('.player').contentWindow.document : document;
-  
   var event = new MouseEvent('click', {
     'view': window,
     'bubbles': true,
     'cancelable': true
   })
-  ,ele = par.querySelector('.bilibili-player-video-btn-danmaku');
-  setTimeout(function(){
+    , ele = par.querySelector('.bilibili-player-video-btn-danmaku');
+  setTimeout(function () {
     ele.dispatchEvent(event);
-  },300);
+  }, 300);
   // var offed = -1 != ele.getAttribute("class").split(" ").indexOf("video-state-danmaku-off");
   chrome.extension.sendRequest({ greeting: "hello" }, function (response) {
-
-
   });
 }
+
 // 创建观察者对象
 observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
-
-    // if (mutation.type == "childList") {
-    //   mutation.addedNodes.forEach(function (node) {
-    //     var danmakuBtnFound = node.nodeType == 1 && node.hasAttribute("class") && -1 != node.getAttribute("class").split(" ").indexOf(danmakuBtn.slice(1));
-
-    //     if (danmakuBtnFound) {
-    //       con1 = true;
-    //     }
-    //   })
-    // }
     mutation.removedNodes.forEach(function (node) {
       var removedLoadWrap = node.nodeType == 1 && node.hasAttribute("class") && -1 != node.getAttribute("class").split(" ").indexOf("bilibili-player-video-load-wrap");
       if (removedLoadWrap) {
         con2 = true;
-
-
       }
     })
   });
 
-  if ( con2) {
+  if (con2) {
     observer.disconnect();
 
     closeDanmaku();
   }
 });
 
+function onPageChanged() {
+  con2 = false;
+  observer.observe(target, config);
+}
 
+function closeAndDisconnect() {
+  closeDanmaku()
+  observer.disconnect();
+}
+
+$(document).on("click", ".episode-item,.bilibili-player-video-toast-item-jump", closeAndDisconnect);
 // 配置观察选项:
 var config = { childList: true, characterData: true, subtree: true }
 
 observer.observe(target, config);
 
-
-
-window.addEventListener("hashchange", function () {
- 
-    con2 = false;
-    observer.observe(target, config);
-  
-  
-}, false);
+window.addEventListener("hashchange", onPageChanged, false);
